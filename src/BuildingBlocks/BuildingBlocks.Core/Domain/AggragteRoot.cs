688600a5f69e7b066394537028dc631e49db1ec0
brunodias77 <brunohdias95@gmail.com>
2025-10-31 10:43:52 -0300

    ...

--- src/BuildingBlocks/BuildingBlocks.Core/Domain/AggragteRoot.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Domain/AggragteRoot.cs
@@ -1,6 +1,25 @@
+using BuildingBlocks.CQRS.Events;
+
 namespace BuildingBlocks.Core.Domain;
 
 public abstract class AggragteRoot : Entity
 {
-    
+    private readonly List<IDomainEvent> _domainEvents = new();
+
+    public IReadOnlyCollection<IDomainEvent> DomainEvents => _domainEvents.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RemoveDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Remove(domainEvent);
+    }
 }
--- src/BuildingBlocks/BuildingBlocks.Core/Domain/ValueObject.cs
+++ src/BuildingBlocks/BuildingBlocks.Core/Domain/ValueObject.cs
@@ -1,6 +1,50 @@
 namespace BuildingBlocks.Core.Domain;
 
-public abstract class ValueObject
+public abstract class ValueObject : IEquatable<ValueObject>
 {
-    
+    protected abstract IEnumerable<object?> GetEqualityComponents();
+
+    public bool Equals(ValueObject? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        if (GetType() != other.GetType()) return false;
+
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as ValueObject);
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Where(x => x != null)
+            .Aggregate(1, (current, obj) => current * 23 + obj!.GetHashCode());
+    }
+
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+    {
+        return !Equals(left, right);
+    }
+
+    protected static bool EqualOperator(ValueObject? left, ValueObject? right)
+    {
+        if (left is null ^ right is null)
+            return false;
+
+        return left?.Equals(right) != false;
+    }
+
+    protected static bool NotEqualOperator(ValueObject? left, ValueObject? right)
+    {
+        return !EqualOperator(left, right);
+    }
 }
